e3a4cd6c81a23f89b3d6d58fb95c224c11f5b091
premmd94 <premkumar.md94@gmail.com>
2017-09-08 16:59:10 +0530

    latest

--- src/app/youtube/youtube.component.ts
+++ src/app/youtube/youtube.component.ts
@@ -1,13 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+import { slideInOutAnimation } from '../animations/slide-in.animatios';
+declare var $: any;
+declare var WOW: any;
 
 @Component({
   selector: 'app-youtube',
   templateUrl: './youtube.component.html',
-  styleUrls: ['./youtube.component.css']
+  styleUrls: ['./youtube.component.css'],
+  animations: [slideInOutAnimation],
+  host: { '[@slideInOutAnimation]': '' }
 })
 export class YoutubeComponent implements OnInit {
 
-  constructor() { }
+  youtube: FirebaseListObservable<any[]>;
+  constructor(db: AngularFireDatabase) {
+    this.youtube = db.list('/youtube');
+  }
 
   ngOnInit() {
   }
